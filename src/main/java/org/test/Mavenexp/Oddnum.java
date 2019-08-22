2fec4400de64548e26e4f52613a1bcfc53e83f7e
Vidhyagunashri <vidhyasraji@gmail.com>
2019-08-22 15:50:42 +0530

    comments

---
+++ src/main/java/org/test/Mavenexp/Oddnum.java
@@ -0,0 +1,13 @@
+package org.test.Mavenexp;
+
+public class Oddnum {
+	public static void main(String[] args) {
+		for(int i=0;i<20;i++)
+			if(i%2==1)
+			{
+				System.out.println("Enter the odd num");
+				System.out.println(i);
+			}
+	}
+
+}
